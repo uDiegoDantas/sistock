1540ad340b228ed8eb007e69e6804e701fd6185b
joaosantosmedeiros <jopesame@gmail.com>
2025-04-15 18:27:44 -0300

    feat: add caso de uso de deleção de categoria (#20)

---
+++ estoque-back/src/application/usecases/category/delete-category.usecase.spec.ts
@@ -0,0 +1,23 @@
+import { makeCategory } from '@test/factories/category.factory';
+import { DeleteCategoryUseCase } from './delete-category.usecase';
+import { InMemoryCategoryRepository } from '@test/repositories/in-memory-category.repository';
+
+describe('DeleteCategoryUseCase', () => {
+  it('should delete an category correctly', async () => {
+    const categoryRepository = new InMemoryCategoryRepository();
+    const deleteCategory = new DeleteCategoryUseCase(categoryRepository);
+
+    categoryRepository.create(makeCategory());
+    const category = categoryRepository.categories[0];
+
+    await deleteCategory.execute(category.id!);
+    expect(categoryRepository.categories.length).toBe(0);
+  });
+
+  it('should not delete an unexistent category', async () => {
+    const categoryRepository = new InMemoryCategoryRepository();
+    const deleteCategory = new DeleteCategoryUseCase(categoryRepository);
+
+    await expect(deleteCategory.execute(-999)).rejects.toThrow(Error);
+  });
+});
