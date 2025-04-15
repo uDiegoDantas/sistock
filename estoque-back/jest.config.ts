63de5af2af27a41b15b9ea7d4ed804b426890719
joaosantosmedeiros <jopesame@gmail.com>
2025-04-15 17:50:00 -0300

    chore: add arquivos de configuração de projeto (#20)

---
+++ estoque-back/jest.config.ts
@@ -0,0 +1,33 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/*.module.*',
+    '!**/*.controller.*',
+    '!**/*.service.*',
+    '!**/node_modules/**',
+    '!**/dist/**',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  modulePathIgnorePatterns: [
+    './docker-data/*',
+    './src/application/repositories/*',
+    './src/enums/*',
+    './src/infra/*',
+    './test/factories/*',
+  ],
+};
+
+export default config;
