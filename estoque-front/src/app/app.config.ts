3282c731ee532bdbfed9c140dbb13eb867ef48fb
joaosantosmedeiros <jopesame@gmail.com>
2025-05-03 16:56:07 -0300

    fix: correcao no app config (#20)

--- estoque-front/src/app/app.config.ts
+++ estoque-front/src/app/app.config.ts
@@ -6,11 +6,11 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
