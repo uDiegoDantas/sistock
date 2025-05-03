32e89b02720569af349094be2a63692ca5d7cb4c
joaosantosmedeiros <jopesame@gmail.com>
2025-05-03 16:57:08 -0300

    feat: add pagina de categorias (#20)

--- estoque-front/src/app/app.routes.ts
+++ estoque-front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CategoryComponent } from './pages/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { component: CategoryComponent, path: 'category' },
+];
