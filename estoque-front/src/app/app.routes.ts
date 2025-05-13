b0a01130edb2b1b051108cfe8a12745d70f2bc77
Kingnaldo1337 <reinaldo20jr@gmail.com>
2025-05-13 14:41:39 -0300

    feat: add pagina de produtos (#22)

--- estoque-front/src/app/app.routes.ts
+++ estoque-front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from './pages/category/category.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
   { component: CategoryComponent, path: 'category' },
+  { component: ProductComponent, path: 'product' },
 ];
