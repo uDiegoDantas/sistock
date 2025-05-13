27bdd0ea0e04fa4327ee7fd507971160fdc3bb40
Kingnaldo1337 <reinaldo20jr@gmail.com>
2025-05-13 14:09:46 -0300

    feat: add entidade base de produto (#22)

---
+++ estoque-back/src/application/entities/product.spec.ts
@@ -0,0 +1,47 @@
+import { Category } from './category';
+import { Product } from './product';
+
+describe('Product', () => {
+  it('should be able to create a new product', () => {
+    const product = new Product({
+      name: 'any_name',
+      categoryId: 0,
+      price: 25.5,
+    });
+
+    expect(product).toBeTruthy();
+  });
+
+  it('should create a new product with correct values', () => {
+    const product = new Product({
+      name: 'any_name',
+      categoryId: 0,
+      price: 25.5,
+    });
+
+    expect(product.id).toBeUndefined();
+    expect(product.name).toBe('any_name');
+  });
+
+  it('should update product with correct values', () => {
+    const product = new Product({
+      name: 'any_name',
+      categoryId: 0,
+      price: 25.5,
+    });
+
+    const name = 'another_name';
+    const price = 12.75;
+    const anotherCategory = new Category({ name: 'another_category' }, 1);
+
+    product.name = name;
+    product.price = price;
+    product.category = anotherCategory;
+
+    expect(product.name).toBe(name);
+    expect(product.price).toBe(price);
+    expect(product.category).toBe(anotherCategory);
+    expect(product.categoryId).toBe(anotherCategory.id);
+    expect(product.id).toBeUndefined();
+  });
+});
