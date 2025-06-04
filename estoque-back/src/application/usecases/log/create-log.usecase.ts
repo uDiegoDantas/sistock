6d63b7ef71b63d81f0d8b21d4a163853c85a8bac
Simio123 <emerson.santos.125@ufrn.edu.br>
2025-06-04 16:55:07 -0300

    feat: add create log usecase (#36)

---
+++ estoque-back/src/application/usecases/log/create-log.usecase.ts
@@ -0,0 +1,25 @@
+import { Log } from '@application/entities/log';
+import { Stock } from '@application/entities/stock';
+import { LogRepository } from '@application/repositories/log.repository';
+import { Injectable } from '@nestjs/common';
+
+export interface CreateLogRequest {
+  date: Date;
+  quantity: number;
+  stock: Stock;
+}
+
+@Injectable()
+export class CreateLogUseCase {
+  constructor(private readonly logRepository: LogRepository) {}
+
+  async execute(request: CreateLogRequest): Promise<Log> {
+    const log: Log = new Log({
+      date: new Date(request.date),
+      quantity: request.quantity,
+      stockId: request.stock.id!,
+    });
+
+    return this.logRepository.create(log);
+  }
+}
