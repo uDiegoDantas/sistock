3b620f7f97a36125dd2e7e968a07c6d368db8566
joaosantosmedeiros <jopesame@gmail.com>
2025-06-11 20:08:18 -0300

    add componente de confirmacao de mensagem (#40)

---
+++ estoque-front/src/app/shared/components/message-dialog-confirm/message-dialog-confirm.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, Optional } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-message-dialog-confirm',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogTitle
+  ],
+  templateUrl: './message-dialog-confirm.component.html',
+})
+export class MessageDialogConfirmComponent {
+
+  constructor(@Optional() @Inject(MAT_DIALOG_DATA) public msg: string) { }
+
+}
