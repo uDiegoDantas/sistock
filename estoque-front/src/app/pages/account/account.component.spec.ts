36f807030248629cbd2d541ddb3421947927bda8
Emerson Santos <134620060+Simio123@users.noreply.github.com>
2025-06-20 16:48:58 -0300

    criação do arquivo de testes do componente Account

---
+++ estoque-front/src/app/pages/account/account.component.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AccountComponent } from './account.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthService } from '../../shared/services/auth.service';
+import { AccountService } from './service/account.service';
+import { SnackbarService } from '../../shared/services/snackbar.service';
+import { UtilsService } from '../../shared/services/utils.service';
+import { of, throwError } from 'rxjs';
+import { Account } from '../../shared/models/account';
+
+describe('AccountComponent', () => {
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
+
+  let accountServiceSpy: jasmine.SpyObj<AccountService>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let snackbarServiceSpy: jasmine.SpyObj<SnackbarService>;
+  let utilsServiceSpy: jasmine.SpyObj<UtilsService>;
+
+  const mockAccounts: Account[] = [
+    { id: 1, name: 'Admin', userType: 0, isActive: true },
+    { id: 2, name: 'Inativo', userType: 1, isActive: false },
+  ];
+
+  beforeEach(() => {
+    accountServiceSpy = jasmine.createSpyObj('AccountService', ['list', 'create', 'delete']);
+    authServiceSpy = jasmine.createSpyObj('AuthService', [], { isAdmin$: of(true) });
+    snackbarServiceSpy = jasmine.createSpyObj('SnackbarService', ['open']);
+    utilsServiceSpy = jasmine.createSpyObj('UtilsService', ['openDialog', 'onError']);
+
+    TestBed.configureTestingModule({
+      imports: [AccountComponent],
+      providers: [
+        provideHttpClientTesting(),
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: AccountService, useValue: accountServiceSpy },
+        { provide: SnackbarService, useValue: snackbarServiceSpy },
+        { provide: UtilsService, useValue: utilsServiceSpy },
+      ],
+    });
+
+    fixture = TestBed.createComponent(AccountComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  
+});
