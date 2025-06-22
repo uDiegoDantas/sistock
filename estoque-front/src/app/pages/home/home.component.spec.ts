6c5b6c95d17ae8d548497f22db6d26991c9c4338
Diego Dantas <diegodantasjsrn@gmail.com>
2025-06-22 17:24:34 -0300

    test: add teste secao de login

--- estoque-front/src/app/pages/home/home.component.spec.ts
+++ estoque-front/src/app/pages/home/home.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { AuthService } from '../../shared/services/auth.service';
+import { BehaviorSubject } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+  let isLoggedInSubject: BehaviorSubject<boolean>;
+  let authServiceMock: Partial<AuthService>;
+
+  beforeEach(() => {
+    isLoggedInSubject = new BehaviorSubject<boolean>(false);
+    authServiceMock = {
+      isLoggedIn$: isLoggedInSubject.asObservable(),
+    };
+
+    TestBed.configureTestingModule({
+      imports: [HomeComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }, provideRouter([])],
+    });
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show login section when user is NOT logged in', () => {
+    isLoggedInSubject.next(false);
+    fixture.detectChanges();
+
+    const loginSection = fixture.debugElement.query(By.css('.login'));
+    expect(loginSection).toBeTruthy();
+
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button).toBeTruthy();
+    expect(button.nativeElement.textContent.trim()).toBe('Login');
   });
 });
