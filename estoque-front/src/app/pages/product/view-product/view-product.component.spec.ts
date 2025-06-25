07a4c57bb4c6dd34c9f97c3cecce48926f7c658f
Reinaldo Alves <102365824+Kingnaldo1337@users.noreply.github.com>
2025-06-25 10:31:30 -0300

    cria o produto, inicializa ele, e carrega o estoque na inicialização

--- estoque-front/src/app/pages/product/view-product/view-product.component.spec.ts
+++ estoque-front/src/app/pages/product/view-product/view-product.component.spec.ts
@@ -1,23 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViewProductComponent } from './view-product.component';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { UtilsService } from '../../../shared/services/utils.service';
+import { StockService } from '../service/stock.service';
+import { of, throwError } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Category } from '../../../shared/models/category';
+import { Product } from '../../../shared/models/product';
+import { Stock } from '../../../shared/models/stock';
 
 describe('ViewProductComponent', () => {
   let component: ViewProductComponent;
   let fixture: ComponentFixture<ViewProductComponent>;
 
+  let stockServiceSpy: jasmine.SpyObj<StockService>;
+  let utilsServiceSpy: jasmine.SpyObj<UtilsService>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<ViewProductComponent>>;
+
+  const category: Category = { id: 0, name: 'C1', isActive: true };
+  const product: Product = { id: 0, name: 'p1', category, price: 11, isActive: true };
+  const stock: Stock = { id: 10, product, quantity: 42 };
+
   beforeEach(async () => {
+    stockServiceSpy = jasmine.createSpyObj('StockService', ['findByProduct']);
+    utilsServiceSpy = jasmine.createSpyObj('UtilsService', ['onError', 'convertToReal']);
+    dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      imports: [ViewProductComponent]
-    })
-    .compileComponents();
+      imports: [ViewProductComponent, MatDialogModule, MatIconModule, MatButtonModule],
+      providers: [
+        { provide: StockService, useValue: stockServiceSpy },
+        { provide: UtilsService, useValue: utilsServiceSpy },
+        { provide: MatDialogRef, useValue: dialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: { product: product } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ViewProductComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and initialize product', () => {
+    stockServiceSpy.findByProduct.and.returnValue(of(stock));
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+    expect(component.product).toEqual(product);
   });
-});
+
+  it('should load stock on init', () => {
+    stockServiceSpy.findByProduct.and.returnValue(of(stock));
+    fixture.detectChanges();
